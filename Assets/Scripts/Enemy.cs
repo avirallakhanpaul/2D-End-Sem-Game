80972d517afe98bb6f59903dfaea1715569c5f5a
avirallakhanpaul <avirallakhanpaul8@gmail.com>
2020-11-11 18:53:04 +0530

    Revert "Added Prefab Change while Switching Game State"
    This reverts commit 9d0d7237a481519dd387e3e6a389a8b53bc17d16.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,35 +6,17 @@ public class Enemy : MonoBehaviour {
 
     public Transform axleMidPoint;
     public float movementSpeed;
-    public float speedIncValue;
-    public float temp;
-    public GameManager gm;
+    public GameManager gameManager;
 
     void Start() {
 
-        gm = FindObjectOfType<GameManager>();
-        axleMidPoint = GameObject.FindGameObjectWithTag("axle-mid-point").transform;  
-        temp = movementSpeed;
+        axleMidPoint = GameObject.FindGameObjectWithTag("axle-mid-point").transform;    
     }
 
     void Update() {
-
-        // Debug.Log(movementSpeed);
-
-        // if(gm.score >= 5) {
-
-        //     if(!(movementSpeed > temp)) {
-        //         temp = movementSpeed;
-        //         increaseSpeed();
-        //     }
-        // }
         
         transform.position = Vector3.MoveTowards(transform.position, axleMidPoint.position, movementSpeed * Time.deltaTime);
         transform.Rotate(0, 0, 60 * Time.deltaTime);
-    }
-
-    void increaseSpeed() {
-        movementSpeed += speedIncValue;
     }
 
     void OnTriggerEnter2D(Collider2D obj) {
@@ -42,10 +24,7 @@ public class Enemy : MonoBehaviour {
         if(obj.CompareTag("axle")) {
 
             Destroy(gameObject);
-            gm.score++;
-        } else if(obj.CompareTag("ball-player")) {
-            Destroy(gameObject);
-            gm.isGameOver = true;
+            gameManager.score++;
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,22 +10,15 @@ public class GameManager : MonoBehaviour {
     public bool isGameStateDefending; 
     private float screenWidth;
     private float screenHeight;
-    public int score = 0;
+    public int score;
     public Text scoreText;
     public GameObject spike;
     public GameObject ball;
-    private GameObject enemy;
+    private GameObject spikeEnemy;
     public float interval;
-    public bool isGameOver;
-    GameObject[] ballPlayerPrefabs;
-    GameObject[] spikePlayerPrefabs; 
-    // GameObject[] gamePrefabs;
 
     void Start() {
 
-        ballPlayerPrefabs = GameObject.FindGameObjectsWithTag("ball-player");
-        spikePlayerPrefabs = GameObject.FindGameObjectsWithTag("spike-player");
-        
         isGameStateDefending = true;
 
         screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
@@ -35,30 +28,9 @@ public class GameManager : MonoBehaviour {
     void InstantiateEnemy() {
 
         if(isGameStateDefending) {
-
-            foreach (GameObject ballPlayerPrefab in ballPlayerPrefabs) {
-                ballPlayerPrefab.SetActive(true);
-            }
-            
-            foreach (GameObject spikePlayerPrefab in spikePlayerPrefabs) {
-                spikePlayerPrefab.SetActive(false);
-            }
-
-            enemy = spike;
-
+            spikeEnemy = spike;
         } else if(!isGameStateDefending) {
-
-            Debug.Log("Attack Mode!");
-
-            foreach (GameObject spikePlayerPrefab in spikePlayerPrefabs) {
-                spikePlayerPrefab.SetActive(true);
-            }
-
-            foreach (GameObject ballPlayerPrefab in ballPlayerPrefabs) {
-                ballPlayerPrefab.SetActive(false);
-            }
-
-            enemy = ball;
+            spikeEnemy = ball;
         }
 
         int range = Random.Range(1, 5);
@@ -68,23 +40,23 @@ public class GameManager : MonoBehaviour {
         switch(range) {
 
             case 1:
-                Instantiate(enemy);
-                enemy.transform.position = new Vector2(enemyPositionX, screenBounds.y);
+                Instantiate(spikeEnemy);
+                spikeEnemy.transform.position = new Vector2(enemyPositionX, screenBounds.y);
                 break;
 
             case 2:
-                Instantiate(enemy);
-                enemy.transform.position = new Vector2(screenBounds.x, enemyPositionY);
+                Instantiate(spikeEnemy);
+                spikeEnemy.transform.position = new Vector2(screenBounds.x, enemyPositionY);
                 break;
 
             case 3:
-                Instantiate(enemy);
-                enemy.transform.position = new Vector2(enemyPositionX, -screenBounds.y);
+                Instantiate(spikeEnemy);
+                spikeEnemy.transform.position = new Vector2(enemyPositionX, -screenBounds.y);
                 break;
 
             case 4:
-                Instantiate(enemy);
-                enemy.transform.position = new Vector2(-screenBounds.x, enemyPositionY);
+                Instantiate(spikeEnemy);
+                spikeEnemy.transform.position = new Vector2(-screenBounds.x, enemyPositionY);
                 break;
         }
     }
@@ -92,15 +64,5 @@ public class GameManager : MonoBehaviour {
     void Update() {
 
         scoreText.text = score.ToString();
-
-        if(Input.GetKeyDown(KeyCode.A)) {
-            isGameStateDefending = !isGameStateDefending;
-        }
     }
 }
-
-// Destroy all the game objects except the player when switching between the states
-// Increase the movement speed of the enemies after fixed intervals of time by a fixed amount
-// Add OnTriggerEnter to spike player
-// Change the color of the spike player from white to something else
-// Add Powerups(speed boost, state change)
